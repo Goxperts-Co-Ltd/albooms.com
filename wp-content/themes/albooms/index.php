42ce36d52a530b7bb7e0e05a011feef81c80748b
Ferdinand Lazarte <ferdinand.lazarte@tell.com>
2020-03-15 10:26:06 +0800

    Banner adsense

--- wp-content/themes/albooms/index.php
+++ wp-content/themes/albooms/index.php
@@ -102,12 +102,17 @@ get_header();
 	<!-- Banner section -->
 	<section class="banner-section">
 		<div class="container">
-			<div class="banner set-bg" data-setbg="<?php bloginfo('stylesheet_directory');?>/assets/img/banner-bg.jpg">
-				<div class="tag-new">NEW</div>
-				<span>New Arrivals</span>
-				<h2>STRIPED SHIRTS</h2>
-				<a href="#" class="site-btn">SHOP NOW</a>
-			</div>
+		<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+		<!-- albooms.com  front page -->
+		<ins class="adsbygoogle"
+			style="display:block"
+			data-ad-client="ca-pub-4712491227153267"
+			data-ad-slot="5855057523"
+			data-ad-format="auto"
+			data-full-width-responsive="true"></ins>
+		<script>
+			(adsbygoogle = window.adsbygoogle || []).push({});
+		</script>
 		</div>
 	</section>
 	<!-- Banner section end  -->

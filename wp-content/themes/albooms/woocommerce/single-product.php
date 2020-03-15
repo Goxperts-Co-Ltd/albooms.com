fc391ee3996af2ab4cacad8b384210c7f37a2a97
Ferdinand Lazarte <ferdinand.lazarte@tell.com>
2020-03-15 15:40:53 +0800

    Revisions of products detail page

--- wp-content/themes/albooms/woocommerce/single-product.php
+++ wp-content/themes/albooms/woocommerce/single-product.php
@@ -6,11 +6,9 @@ get_header();
 global $post;
 global $product;
 $terms = get_the_terms( $post->ID, 'product_cat');
-$query = new WC_Product_Query(array(
-	'name' => $product,
-	'type' => 'variable'
-    ));
-$products = $query->get_products();
+$attributes = $product->get_attributes();
+$available_variations = $product->get_available_variations();
+
 ?>
 
 	<!-- Page info -->
@@ -18,7 +16,7 @@ $products = $query->get_products();
 		<div class="container">
 			<h4>Category PAge</h4>
 			<div class="site-pagination">
-				<a href="">Home</a> /
+				<a href="">Home</a> 
 				<a href="">Shop</a>
 			</div>
 		</div>
@@ -32,22 +30,22 @@ $products = $query->get_products();
 			<div class="row">
 				<div class="col-lg-6">
 					<div class="product-pic-zoom">
-						<img class="product-big-img" src="<?php echo wp_get_attachment_image_src( $products[0]->image_id,'full')[0]; ?>" alt="">
+						<img class="product-big-img" src="<?php echo wp_get_attachment_image_src( $product->get_image_id(),'full')[0]; ?>" alt="">
 					</div>
 					<div class="product-thumbs" tabindex="1" style="overflow: hidden; outline: none;">
 						<div class="product-thumbs-track">
 							<!-- Featured Image as default -->
-						<div class="pt active" data-imgbigurl="<?php echo wp_get_attachment_image_src( $products[0]->image_id,'full')[0]; ?>"><img src="<?php echo wp_get_attachment_image_src( $products[0]->image_id,'full')[0]; ?>" alt=""></div>
+						<div class="pt active" data-imgbigurl="<?php echo wp_get_attachment_image_src( $product->get_image_id(),'full')[0]; ?>"><img src="<?php echo wp_get_attachment_image_src( $product->get_image_id(),'full')[0]; ?>" alt=""></div>
 							<!-- Image Galleries -->
-						<?php  foreach($products[0]->gallery_image_ids as $gallery) : ?>				
+						<?php  foreach($product->get_gallery_image_ids() as $gallery) : ?>				
 							<div class="pt" data-imgbigurl="<?php echo wp_get_attachment_image_src( $gallery,'full')[0]; ?>"><img src="<?php echo wp_get_attachment_image_src( $gallery,'full')[0]; ?>" alt=""></div>
 						<?php endforeach; ?>
 						</div>
 					</div>
 				</div>
 				<div class="col-lg-6 product-details">
-					<h2 class="p-title"><?php echo $products[0]->name; ?></h2>
-					<h3 class="p-price"><?php echo $products[0]->price; ?></h3>
+					<h2 class="p-title"><?php echo $product->get_name(); ?></h2>
+					<h3 class="p-price"><?php echo $product->get_price_html(); ?></h3>
 					<!-- <h4 class="p-stock">Available: <span>In Stock</span></h4> -->
 					<div class="p-rating">
 						<i class="fa fa-star-o"></i>
@@ -57,7 +55,7 @@ $products = $query->get_products();
 						<i class="fa fa-star-o fa-fade"></i>
 					</div>
 					<div class="p-review">
-						<a href=""><?php echo $products[0]->review_count;?> reviews</a>|<a href="">Add your review</a>
+						<a href=""><?php echo $product->get_review_count();?> reviews</a>|<a href="">Add your review</a>
 					</div>
 					<div id="accordion" class="accordion-area">
 						<div class="panel">
@@ -67,14 +65,12 @@ $products = $query->get_products();
 							<div id="collapse12" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
 								<div class="panel-body">
 								<div class="col-md-5">		
-							      <?php foreach($products[0]->attributes as $key['sizes'] => $sizes) : ?>
-								    <?php  foreach($sizes['options'] as $option) : ?>
+							      <?php foreach($available_variations as $key => $sizes) : //print_r(); ?>
 									 <div class="cfr-item">
 										<input type="radio" name="pm" id="<?php echo $option; ?>">
-										<label for="<?php echo $option; ?>"><?php echo $option; ?></label>
+										<label for="<?php echo $sizes['attributes']['attribute_sizes']; ?>"><?php echo $sizes['attributes']['attribute_sizes']; ?></label>
 									 </div>
 								    <?php endforeach; ?>
-							     <?php endforeach; ?>		
 						      </div>
 							</div>
 						  </div>
@@ -89,7 +85,7 @@ $products = $query->get_products();
 							</div>
 							<div id="collapse1" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
 								<div class="panel-body">
-									<?php echo $products[0]->description;?>
+									<?php echo $product->get_description(); ?>
 								</div>
 							</div>
 						</div>
@@ -99,7 +95,7 @@ $products = $query->get_products();
 							</div>
 							<div id="collapse2" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
 								<div class="panel-body">
-									<img src="./img/cards.png" alt="">
+									<img src="<?php bloginfo('stylesheet_directory');?>/assets/img/cards.png" alt="">
 									<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin pharetra tempor so dales. Phasellus sagittis auctor gravida. Integer bibendum sodales arcu id te mpus. Ut consectetur lacus leo, non scelerisque nulla euismod nec.</p>
 								</div>
 							</div>
